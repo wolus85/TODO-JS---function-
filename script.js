86e35f018bf775d8424bff80c84eab39ea75d3fa
Paweł Wolski <pawelwolski3@gmail.com>
2019-10-05 13:44:25 +0200

    add append function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const renderContainer = () => {
+const renderContainer = function () {
   return document.createElement('div')
 }
 
 
-const renderTaskCOntainer = (text) => {
+const renderTaskCOntainer = function (text = '') {
   const task = renderContainer()
   task.innerText = text
   task.style.boxShadow = '0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12)'
@@ -13,3 +13,13 @@ const renderTaskCOntainer = (text) => {
 
   return task
 }
+
+const append = function (child,container = document.body) {
+
+  container.appendChild(child)
+}
+
+// append
+// takes 2 params - child, container
+// it should appends child to container
+// *if no container passed container should be equal to document.body
