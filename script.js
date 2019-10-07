111275e9bd2fb334c621b301f743a405e8ad3b8b
Paweł Wolski <pawelwolski3@gmail.com>
2019-10-07 11:16:43 +0200

    change variable names

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const toDoListContainer = document.body
+
 let tasks = [
   { text: 'Wynieś śmieci' },
   { text: 'Zmyj gary' },
@@ -8,24 +10,43 @@ const renderContainer = function () {
   return document.createElement('div')
 }
 
-const renderTaskCOntainer = function (text = '') {
-  const task = renderContainer()
-  task.innerText = text
-  task.style.boxShadow = '0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12)'
-  task.style.borderRadius = '4px'
-  task.style.padding = '10px'
-  task.style.margin = '10px 0px'
+const renderTaskContainer = function (text = '') {
+  const taskContainer = renderContainer()
+  taskContainer.innerText = text
+  taskContainer.style.boxShadow = '0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12)'
+  taskContainer.style.borderRadius = '4px'
+  taskContainer.style.padding = '10px'
+  taskContainer.style.margin = '10px 0px'
 
-  return task
+  return taskContainer
 }
 
 const append = function (child, container = document.body) {
   container.appendChild(child)
 }
 
+
+
+
+
+const renderTaskList = function () {
+const taskListContainer = renderContainer() 
+taskListContainer.style.width = '100%'
+
+tasks.forEach
+((task)=>{const taskContainer = renderTaskContainer(task.text)
+append(taskContainer, taskListContainer)
+})
+
+return taskListContainer
+
+}
+
 const render = function () {
-  const div = renderTaskCOntainer(text = 'wynieś śmieci')
-  append(div)
+  toDoListContainer.innerText = ''
+  const taskList = renderTaskList()
+  append(taskList,toDoListContainer)
 }
 
 render()
+
